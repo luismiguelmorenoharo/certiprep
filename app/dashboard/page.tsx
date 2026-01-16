563ab02716d0c7d80cdf7746581def311cb36c62
Luis Moreno <94276802+luismiguelmorenoharo@users.noreply.github.com>
2026-01-16 18:54:01 +0000

    feat(auth): setup signup, login, dashboard & logout

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { supabase } from '@/lib/supabase'
+import { useRouter } from 'next/navigation'
+
+export default function DashboardPage() {
+  const [user, setUser] = useState<any>(null)
+  const router = useRouter()
+
+  useEffect(() => {
+    const session = supabase.auth.getSession().then(({ data }) => {
+      if (!data.session) {
+        router.push('/login')
+      } else {
+        setUser(data.session.user)
+      }
+    })
+  }, [router])
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut()
+    router.push('/login')
+  }
+
+  if (!user) return <div>Loading...</div>
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Welcome, {user.email}</h1>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  )
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { supabase } from '@/lib/supabase'
+
+export default function LoginPage() {
+  const router = useRouter()
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [message, setMessage] = useState('')
+
+  const handleLogin = async () => {
+    setMessage('') // limpiar mensaje previo
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+
+    if (error) {
+      setMessage(error.message)
+    } else {
+      setMessage('Login successful!')
+      // Redirigir a dashboard
+      router.push('/dashboard')
+    }
+  }
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>CertiPrep – Login</h1>
+
+      <input
+        placeholder="Email"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        placeholder="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={handleLogin}>
+        Login
+      </button>
+
+      <p>{message}</p>
+    </div>
+  )
+}
